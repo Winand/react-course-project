b35a9a41c6b00db1cc4421e380e6c51289872017
Makarov Andrey <winandfx@gmail.com>
2023-04-20 11:55:05 +0300

    search button

--- src/components/EmployeeList/SearchBar/SearchBar.js
+++ src/components/EmployeeList/SearchBar/SearchBar.js
@@ -1,5 +1,14 @@
+import { useCallback, useState } from 'react';
 import './SearchBar.css'
 
 export const SearchBar = () => {
-    return (<input className="search-bar" type="text" placeholder="🔎︎ Search..."/>)
+    const [isSearchDisables, setIsSearchDisables] = useState(false);
+    const doSearch = useCallback(() => {
+        setIsSearchDisables(true);
+        setTimeout(() => {setIsSearchDisables(false)}, 2000);
+    }, [setIsSearchDisables]);
+    return (<div>
+        <input className="search-bar" type="text" placeholder="🔎︎ Search..." disabled={isSearchDisables}/>
+        <button disabled={isSearchDisables} onClick={doSearch}>Find</button>
+    </div>)
 }
