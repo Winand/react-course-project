b0551be481f86a6a0023c7d8d2f73d1b97c6eb13
Makarov Andrey <winandfx@gmail.com>
2023-04-24 17:22:04 +0300

    [WIP] Edit employee fields

--- src/components/EmployeeCard/EmployeeCard.jsx
+++ src/components/EmployeeCard/EmployeeCard.jsx
@@ -1,21 +1,44 @@
 import { useParams } from 'react-router-dom';
 import { useEmployeeFullData } from "../../store/useEmployeeFullData";
 import "./EmployeeCard.css";
-import { useEffect } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 import { formatUserName } from '../../utils'
 import { EmployeeCardElement } from './EmployeeCardElement';
 
 export const EmployeeCard = () => {
+    const [employee, setEmployee] = useState();
     const {employeeId} = useParams();
 
     // переименование data->employee https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Destructuring_assignment#nested_object_and_array_destructuring
-    const {data: employee, isFetching, isLoaded, getEmployee} = useEmployeeFullData();
+    const {data, isFetching, isLoaded, getEmployee} = useEmployeeFullData();
+
+    const prepOnSave = useCallback((fieldName)=>(newValue) => {
+        switch(fieldName) {
+            case 'email':
+                setEmployee((state)=>{return {...state, email: newValue}});
+                break;
+            case 'phone-office':
+                setEmployee((state)=>{
+                    const newState = {...state}
+                    newState.phone.office = newValue;
+                    return newState;
+                });
+                break;
+        }
+        console.log(newValue, fieldName);
+    }, [setEmployee]);
 
     useEffect(()=>{
-        if(!employee && !isFetching && !isLoaded && employeeId) {
+        if(!isFetching && !isLoaded && employeeId) {
             getEmployee(employeeId);
         }
-    }, [employee, isFetching, isLoaded, getEmployee, employeeId]);
+    }, [isFetching, isLoaded, getEmployee, employeeId]);
+
+    useEffect(() => {
+        if(data) {
+            setEmployee(data);
+        }
+    }, [data, setEmployee]);
 
     return (<div className="employee-page-wrapper">
         {employee && (
@@ -27,10 +50,10 @@ export const EmployeeCard = () => {
                     <div className='employee-position'>{employee.position}</div>
                 </div>
             </div>
-            <EmployeeCardElement label="Email" value={employee.email} onEditClick={()=>{}}/>
-            <EmployeeCardElement label="Call office" value={employee.phone.office} onEditClick={()=>{}}/>
-            <EmployeeCardElement label="Call mobile" value={employee.phone.cell} onEditClick={()=>{}}/>
-            <EmployeeCardElement label="SMS" value={employee.phone.sms} onEditClick={()=>{}}/>
+            <EmployeeCardElement label="Email" value={employee.email} onSave={prepOnSave('email')}/>
+            <EmployeeCardElement label="Call office" value={employee.phone.office} onSave={prepOnSave('phone-office')}/>
+            <EmployeeCardElement label="Call mobile" value={employee.phone.cell} onSave={prepOnSave('phone-mobile')}/>
+            <EmployeeCardElement label="SMS" value={employee.phone.sms} onSave={prepOnSave('phone-sms')}/>
         </div>)}
 
         {(!employee && isFetching) && (<>Employee data loading...</>)}
--- src/components/EmployeeCard/EmployeeCardElement.jsx
+++ src/components/EmployeeCard/EmployeeCardElement.jsx
@@ -1,11 +1,28 @@
+import { useCallback, useState, useRef } from 'react'
 import './EmployeeCardElement.css'
 
-export const EmployeeCardElement = ({ label, value, onEditClick }) => {
+export const EmployeeCardElement = ({ label, value, onSave }) => {
+    const [isEdit, setIsEdit] = useState(false);
+    const inputRef = useRef();
+
+    const onSubmit = useCallback(() => {
+        setIsEdit(false);
+        onSave(inputRef.current.value);
+    })
+    const onCancel = useCallback(() => {setIsEdit(false)}, [setIsEdit])
+    const onEdit = useCallback(() => {setIsEdit(true)}, [setIsEdit])
+
     return <div className='info-item-wrapper'>
     <div className="info-item">
         <div className="label">{label}</div>
-        <div className="value">{value}</div>
+        <div className="value">{isEdit ? <input value={value} ref={inputRef} type='text'/> : value}</div>
     </div>
-    <div className="action"><button onClick={onEditClick}>Edit</button></div>
+    <div className="action">
+        {!isEdit && <button onClick={onEdit}>Edit</button>}
+        {isEdit && <>
+        <button onClick={onSubmit}>Save</button>
+        <button onClick={onCancel}>Cancel</button>
+        </>}
+    </div>
 </div>
 }
